d15a52faa3734dc24a9792555856e0642a32d576
Abdelrahman Mohamed <146343312+Abdelrahman-Mohamed-program@users.noreply.github.com>
2025-08-09 02:53:42 +0300

    error handling for server
    added error handling middlewares

---
+++ backend/middlewares/errorHandle.js
@@ -0,0 +1,25 @@
+//Error handling middlewares
+const notFound = (req,res,next)=>{
+    let error = new Error()
+    error.message = "404 Not found, this route is not available. Check the method and path again."
+    error.status = 404
+    next(error)
+}
+
+
+const errorHandler = (error,req,res,next)=>{
+    const status = error.status||500
+    res.status(status).json({
+        error:error.message
+    })
+}
+
+
+
+
+
+
+
+module.exports = {
+    notFound,errorHandler
+}
--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,16 @@ const express = require("express"),
   app = express(),
   port = process.env.PORT
 
+  
 
 
 //Routes and middleware config:
+const {notFound,errorHandler} = require("./middlewares/errorHandle")
+
+
+app.use(notFound);
+
+app.use(errorHandler)
 
 
 
