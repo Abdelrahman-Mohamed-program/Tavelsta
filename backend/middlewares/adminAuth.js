2df350c44894d030a776fcf1917ad3d66af086b0
Abdelrahman Mohamed <146343312+Abdelrahman-Mohamed-program@users.noreply.github.com>
2026-04-03 09:30:10 +0200

    ..

--- backend/middlewares/adminAuth.js
+++ backend/middlewares/adminAuth.js
@@ -1,5 +1,5 @@
 const adminAuth = (req, res, next) => {
-  if (!req.user.isAdmin) {
+  if (!req.user?.isAdmin) {
     return res.status(403).json({
       error: "Forbidden",
       message: "You are not authorized to perform this action",
